171472b3d487edb3c85d49e73a582640272ef27b
fitzpaky <kfitzpatrick44@gmail.com>
2020-03-31 19:36:23 -0700

    updating config for test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
 
   var myConfig = {
-    apiKey: config.apiKey,
-    authDomain: config.authDomain,
-    databaseURL: config.databaseURL,
-    projectId: config.projectId,
-    storageBucket: config.storageBucket,
-    messagingSenderId: config.messagingSenderId,
-    appId: config.appId
-  }
+      apiKey: "AIzaSyC9InVKj1sH2d4y2agmXr6p4Vm3qcWZaXM",
+      authDomain: "rps-game-45790.firebaseapp.com",
+      databaseURL: "https://rps-game-45790.firebaseio.com",
+      projectId: "rps-game-45790",
+      storageBucket: "rps-game-45790.appspot.com",
+      messagingSenderId: "988274107263",
+      appId: "1:988274107263:web:f1275e9dd2416da890f04a"
+    };
+    // apiKey: config.apiKey,
+    // authDomain: config.authDomain,
+    // databaseURL: config.databaseURL,
+    // projectId: config.projectId,
+    // storageBucket: config.storageBucket,
+    // messagingSenderId: config.messagingSenderId,
+    // appId: config.appId
+  // }
 
   //   // Initialize Firebase
   firebase.initializeApp(myConfig);
